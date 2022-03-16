4e07bbb70b123cbb8c2da8397fd009821fd5901f
unknown <klagueu@gmail.com>
2022-03-16 19:35:24 +0100

    social v2

---
+++ script.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
+      pp =  body.querySelector('.opaci')
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+    pp.classList.toggle('opaci');
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
